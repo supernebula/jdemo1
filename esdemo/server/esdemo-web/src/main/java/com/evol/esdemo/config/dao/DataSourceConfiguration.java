dcf14d748d2ca266f078f43c243dd3c52469705f
supernebula <supernebula@126.com>
2018-12-09 13:08:16 +0800

    none

--- esdemo/server/esdemo-web/src/main/java/com/evol/esdemo/config/dao/DataSourceConfiguration.java
+++ esdemo/server/esdemo-web/src/main/java/com/evol/esdemo/config/dao/DataSourceConfiguration.java
@@ -4,15 +4,18 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 import java.beans.PropertyVetoException;
 
 @Configuration
 //配置mybatis mapper的扫描路径
 @MapperScan("com.evol.esdemo.dao")
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
 public class DataSourceConfiguration {
 
     //@Value("${spring.datasource.driver}")
-    @Value("${spring.datasource.driver}")
+    @Value("${spring.datasource.driver-class-name}")
     private String jdbcDriver;
 
     //@Value("${spring.datasource.url}")
