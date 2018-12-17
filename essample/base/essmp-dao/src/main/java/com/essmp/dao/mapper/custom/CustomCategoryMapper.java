5422cfa93f56b17acfc1d936c97603d68f035537
supernebula <supernebula@126.com>
2018-12-17 22:22:26 +0800

    none

---
+++ essample/base/essmp-dao/src/main/java/com/essmp/dao/mapper/custom/CustomCategoryMapper.java
@@ -0,0 +1,50 @@
+package com.essmp.dao.mapper.custom;
+
+import com.essmp.entity.Category;
+import com.github.pagehelper.Page;
+
+import java.util.List;
+
+
+/**
+ *
+ */
+public interface CustomCategoryMapper {
+
+    Page<Category> queryByPage();
+
+    /**
+     * 列出类别列表
+     * @return categoryList
+     */
+    List<Category> queryCategory();
+
+    /**
+     *根据Id获取具体类别
+     * @param categoryId
+     * @return category
+     */
+    Category findCategoryById(int categoryId);
+
+    /**
+     * 插入类别信息
+     * @param category
+     * @return
+     */
+    int insertCategory(Category category);
+
+    /**
+     * 更新类别信息
+     * @param category
+     * @return
+     */
+    int updateCategory(Category category);
+
+    /**
+     * 根据Id删除具体类别
+     * @param id
+     * @return
+     */
+    int deleteCategory(int id);
+
+}
