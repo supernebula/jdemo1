dca86d5dd92a0d2e6f4e7cdde7c8eb252ed11086
supernebula <supernebula@126.com>
2018-12-17 20:55:24 +0800

    none

---
+++ essample/server/es-web/src/main/java/com/evol/esweb/EsWebApplication.java
@@ -0,0 +1,14 @@
+package com.evol.esweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsWebApplication.class, args);
+    }
+
+}
+
