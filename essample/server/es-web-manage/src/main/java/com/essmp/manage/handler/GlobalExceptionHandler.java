f77c8312011b2d82517fae537a2164220b621607
supernebula <supernebula@126.com>
2018-12-18 13:29:39 +0800

    none

---
+++ essample/server/es-web-manage/src/main/java/com/essmp/manage/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.essmp.manage.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice //Controller通知
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value=Exception.class)
+    @ResponseBody
+    private Map<String, Object> exceptionHandler(HttpServletRequest req, Exception e)
+    {
+        Map<String, Object> modelMap = new HashMap<>();
+        modelMap.put("success", false);
+        modelMap.put("errMsg", e.getMessage());
+        modelMap.put("stackTrace", e.getStackTrace());
+        modelMap.put("ex", e.toString());
+        return modelMap;
+    }
+}
