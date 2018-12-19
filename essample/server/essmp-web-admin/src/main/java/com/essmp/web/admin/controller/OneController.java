3936781c5da4c0a9ed0a80e912f94bebc80b1027
supernebula <supernebula@126.com>
2018-12-19 22:01:59 +0800

    none

---
+++ essample/server/essmp-web-admin/src/main/java/com/essmp/web/admin/controller/OneController.java
@@ -0,0 +1,16 @@
+package com.essmp.web.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OneController {
+
+    @RequestMapping(value = "/one", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello One";
+    }
+}
