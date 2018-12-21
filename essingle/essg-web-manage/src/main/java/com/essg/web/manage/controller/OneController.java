56ac802dc6de6f0b7c701e12f9bd8fd54859d22b
supernebula <supernebula@126.com>
2018-12-21 14:01:26 +0800

    empty web-manage  run success (spring boot、mybatis、tf、jdbc)、one restcontroller

---
+++ essingle/essg-web-manage/src/main/java/com/essg/web/manage/controller/OneController.java
@@ -0,0 +1,13 @@
+package com.essg.web.manage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OneController {
+    @RequestMapping(value = "/one", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello One";
+    }
+}
