07db806c026e0b3b4ce2e4e578422b1996c58c53
supernebula <supernebula@126.com>
2018-12-30 20:55:44 +0800

    none

--- essingle/essg-web-manage/src/main/java/com/essg/web/manage/controller/api/UserApiController.java
+++ essingle/essg-web-manage/src/main/java/com/essg/web/manage/controller/api/UserApiController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.essg.common.util.*;
+
 import javax.print.DocFlavor;
 
 @RestController
@@ -13,8 +15,8 @@ public class UserApiController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(String username, String password) throws Exception {
         //
-        if(NULL)
-        throw new Exception("未实现的方法");
+        if(StringUtil.isNullOrEmpty(username) || StringUtil.isNullOrEmpty(password))
+            throw new Exception("未实现的方法");
         return "";
     }
 }
